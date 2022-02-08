cf79013cee5f736f4a9819b7aab15394ac0cb257
Ali Yazbek <59251976+TheGreatFable@users.noreply.github.com>
2022-02-08 22:45:39 +0200

    Add files via upload

---
+++ repos/ConsoleApp3/ConsoleApp3/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+    Dim x As Integer
+    Dim y As Integer
+    Function maxi(a As Integer, b As Integer) As Integer
+        x = Console.ReadLine
+        y = Console.ReadLine
+        If a > b Then
+            Return a
+        Else
+            Return b
+        End If
+        Console.WriteLine(maxi(x, y))
+    End Function
+End Module
---
+++ repos/ConsoleApp4/ConsoleApp4/Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub maximum(n_count As Integer)
+        '..init
+        Dim i As Integer
+        Dim maxi As Integer
+        Dim a As Integer
+        maxi = -2 ^ 16
+        Console.Clear()
+        Console.WriteLine("enter " & n_count & " numbers")
+        '...
+        If n_count < 2 Then
+            MsgBox(0)
+        Else
+            For i = 1 To n_count
+                a = Console.ReadLine()
+                If a > maxi Then maxi = a
+            Next
+            '..output
+            Console.WriteLine("the largest is " & maxi)
+            MsgBox("ok?")
+        End If
+    End Sub
+    Sub main()
+        '..maxi...
+        Dim n As Integer
+        n = InputBox("enter the number of items ")
+        maximum(n)
+        maximum(5)
+    End Sub
+End Module
