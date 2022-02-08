a1a8645140fb10dec9b9e8ccd7e5c1313341c616
Ali Yazbek <59251976+TheGreatFable@users.noreply.github.com>
2022-02-08 22:57:58 +0200

    Add files via upload

---
+++ repos/ConsoleApp15/ConsoleApp1/Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim maxnum, minnum, sumnum, countnum, a, avgvalue As Integer
+        maxnum = -2 ^ 16
+        minnum = (-1) * maxnum
+        sumnum = 0
+        countnum = 0
+        a = InputBox("input numbers or 0 to end")
+        While a <> 0
+            If a > maxnum Then maxnum = a
+            If a < minnum Then minnum = a
+            sumnum = sumnum + a
+            countnum = countnum + 1
+            a = InputBox("input numbers or 0 to end")
+        End While
+        If countnum > 0 Then
+            avgvalue = sumnum / countnum
+            Console.WriteLine("the average is:")
+            Console.WriteLine(avgvalue)
+            Console.WriteLine("the maximum is:")
+            Console.WriteLine(maxnum)
+            Console.WriteLine("the minimum is:")
+            Console.WriteLine(minnum)
+            MsgBox(":)")
+        Else
+            MsgBox("error")
+        End If
+    End Sub
+
+End Module
---
+++ repos/ConsoleApp15/ConsoleApp1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ConsoleApp1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ConsoleApp1")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d94166d6-cf81-4b13-b427-c296e5bb6453")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
