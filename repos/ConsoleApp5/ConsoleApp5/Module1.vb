8bf2d791c96464a540238e75f21b5ebe2ab18ffc
Ali Yazbek <59251976+TheGreatFable@users.noreply.github.com>
2022-02-08 22:48:14 +0200

    Add files via upload

---
+++ repos/ConsoleApp5/ConsoleApp5/Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+    Sub main()
+        Console.WriteLine("Discount on total amount in invoices...rules: clients are divided... 1and 2 ...")
+        Dim x As Integer
+        Dim y As Integer
+        Console.WriteLine("the discount is:")
+        Console.WriteLine(disc(x, y))
+        MsgBox(":)")
+    End Sub
+    Function disc(amount As Single, clientcode As Integer) As Single
+        Dim discount As Single
+        amount = InputBox("input the amount")
+        clientcode = InputBox("input the client code 1 or 2")
+        If clientcode = "1" Then
+            If amount > 1000 Then
+                discount = amount * 0.15
+            Else
+                discount = amount * 0.1
+            End If
+        Else
+            If amount > 1000 Then
+                discount = amount * 0.1
+            Else
+                discount = 0
+            End If
+        End If
+        Return discount
+    End Function
+
+
+End Module
---
+++ repos/ConsoleApp6/ConsoleApp6/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+    'given a number n, ouptut a- the last l digits: ld   b- the first f digits:fd
+    'input n l f output ld fd
+    Sub Main()
+        Dim n As Single, l As Single, f As Single, ld As Single, ll As Single, fd As Single
+        n = Console.ReadLine
+        l = Console.ReadLine
+        f = Console.ReadLine
+        ld = n Mod 10 ^ l
+        ll = Int(log10(n)) + 1
+        fd = n \ (10 ^ ll)
+        Console.WriteLine("results:")
+        Console.WriteLine(ld)
+        Console.WriteLine(ll)
+        Console.WriteLine(fd)
+        MsgBox("ok")
+    End Sub
+
+End Module
