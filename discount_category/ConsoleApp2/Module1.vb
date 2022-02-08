4a71eda327ecedb2920ab5d35a99ba96a6f7a3b7
Ali Yazbek <59251976+TheGreatFable@users.noreply.github.com>
2022-02-08 22:07:02 +0200

    Add files via upload

---
+++ discount_category/ConsoleApp2/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+    Dim amount As String
+    Dim category As String
+    Dim discount As String
+
+    Sub Main()
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine("Input the total amount")
+        amount = Console.ReadLine
+        Console.WriteLine("Input category 1 or 2")
+        category = Console.ReadLine
+        If category = "1" Then
+            If amount > 1000 Then
+                discount = amount * 0.15
+            Else
+                discount = amount * 0.1
+            End If
+        Else
+            If amount > 1000 Then
+                discount = amount * 0.1
+            End If
+        End If
+        MsgBox(discount)
+    End Sub
+
+End Module
