e54a3d7400731e9ad0a8c72d618ef6a6e2e2d04c
Ali Yazbek <59251976+TheGreatFable@users.noreply.github.com>
2022-02-08 22:59:58 +0200

    Add files via upload

---
+++ repos/ConsoleApp19/ConsoleApp5/Module1.vb
@@ -0,0 +1,46 @@
+﻿Module Module1
+    Dim myArray(999) As Integer
+    Sub Main()
+        init()
+        lookup()
+    End Sub
+    Sub init()
+        For i = 0 To 999
+            myArray(i) = Int(Rnd() * 10000)
+            Console.WriteLine(myArray(i))
+        Next
+    End Sub
+    Sub lookup()
+        Dim myvalue As Integer
+        Dim location As String
+        myvalue = 1
+        While myvalue <> 0
+            Console.WriteLine("input a number from the list above or 0 to end")
+            myvalue = Console.ReadLine
+            If myvalue <> 0 Then
+                location = find(myvalue)
+                Console.WriteLine(myvalue & " is found in " & location)
+            End If
+        End While
+        MsgBox(":)")
+    End Sub
+    Function find(avalue As Integer) As String
+        Dim j As Integer
+        Dim found As Boolean
+        Dim location2 As Integer
+        j = 0
+        found = False
+        While (j <= 999) And found = False
+            If myArray(j) = avalue Then
+                found = True
+                location2 = j
+            End If
+            j = j + 1
+        End While
+        If found Then
+            Return location2
+        Else
+            Return "another universe, not in here"
+        End If
+    End Function
+End Module
---
+++ repos/ConsoleApp20/ConsoleApp6/Module1.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+    Public word As String, age As String, year As Single, j As Single
+    Sub Main()
+        Console.WriteLine("Hello Mr. Ali")
+        Console.WriteLine("How may i serve you today sir?")
+        j = 0
+        word = Console.ReadLine()
+        While word <> "no"
+            If word = "hack the world" Then Console.WriteLine("i can't do this now, sry")
+            If word = "hack my university" Then Console.WriteLine("i don't have the authority to do this")
+            If word = "what is my name?" Then Console.WriteLine("Your full name is Yazbek Ali")
+            If word = "surprise me" Then
+                Console.WriteLine("would you like to know your age in the future?")
+                age = Console.ReadLine()
+                If age = "yes" Then
+                    Console.WriteLine("please enter the year:")
+                    year = Console.ReadLine
+                    Console.WriteLine("your age will be " & year - 1997)
+                End If
+            End If
+            If word = "i want to fuck you" Then Console.WriteLine("go fuck your self, Suka")
+            j = j + 1
+            If j = 5 Then Console.WriteLine(" if you will ask more, ia tebia viebu v jopu suka bliat")
+            Console.WriteLine("may i help you with something else?")
+            word = Console.ReadLine()
+        End While
+        MsgBox("goodbye sir, see you later.")
+    End Sub
+
+End Module
