e7537110a504420d81146bdf6f3d0dfea3b7d46b
Ali Yazbek <59251976+TheGreatFable@users.noreply.github.com>
2022-02-08 22:53:43 +0200

    Add files via upload

---
+++ repos/ConsoleApp12/ConsoleApp3/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x As Integer
+        x = InputBox("input an amount")
+        Console.WriteLine("the discount is:")
+        Console.WriteLine(Disc(x))
+        MsgBox(":)")
+    End Sub
+    Function Disc(amount As Integer) As Single
+        Dim discount As Integer
+        If amount <= 1000 Then
+            discount = amount * 0.1
+        Else
+            discount = 100 + (amount - 1000) * 0.2
+        End If
+        Return discount
+    End Function
+End Module
