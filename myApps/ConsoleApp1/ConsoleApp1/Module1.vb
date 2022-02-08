e6f3beabd21bc131213c34e7c98ab24315c4205b
Ali Yazbek <59251976+TheGreatFable@users.noreply.github.com>
2022-02-08 22:41:06 +0200

    Add files via upload

---
+++ myApps/ConsoleApp1/ConsoleApp1/Module1.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+
+        '--------------------------
+
+        Dim sep, family, fullname As String
+        fullname = "Ali Yazbek"
+        sep = InStr(fullname, " ")
+        family = Mid(fullname, sep + 1, Len(fullname) - sep)
+        MsgBox(family)
+
+        '--------------------------
+
+        MsgBox(Asc("A"))
+        MsgBox(AscW("Пиер Хорассанжиан"))
+
+
+        '--------------------------
+
+        Dim L As Integer
+        L = 5
+        L = L = 5
+        MsgBox(L)
+
+
+
+    End Sub
+
+End Module
---
+++ myApps/ConsoleOther1/ConsoleApp1/Module1.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim pp1, pp2, a, b, c As String
+        pp1 = InStr("what is this string?", "is")
+        pp2 = InStr(8, "what is this string?", "is")
+        MsgBox(pp1)
+
+        a = "hello world!"
+        a = Left(a, 11)
+        MsgBox(a)
+
+        b = "i want to break free!"
+        b = Mid(b, 3, 4)
+        MsgBox(b)
+
+        c = "                        immm halleen in lovveeee                 "
+        c = Trim(c)
+        MsgBox(c)
+
+        Dim i As Integer
+
+        For i = 0 To 5
+            Console.WriteLine(i)
+        Next
+        MsgBox(":)")
+
+
+
+
+
+    End Sub
+
+
+End Module
