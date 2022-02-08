62954db0b00af13b3b8af8a2aae250f132af6de9
Ali Yazbek <59251976+TheGreatFable@users.noreply.github.com>
2022-02-08 23:54:27 +0200

    Add files via upload

---
+++ MIS440 - Ali Yazbeck/Reformat/WindowsApp1/Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FileOpen(9, CliFile, OpenMode.Append)
+        cmbCliCat.Items.Add("Whole Sales")
+        cmbCliCat.Items.Add("Retailer")
+        cmbCliCat.Items.Add("Other")
+
+
+    End Sub
+
+    Private Sub cmdNew_Click(sender As Object, e As EventArgs) Handles cmdNew.Click
+
+        CliID = Val(txtCliID.Text)
+        CliName = Trim(txtCliName.Text)
+        CliAdress = Trim(txtCliAdress.Text)
+        CliPhone = Trim(txtCliPhone.Text)
+        CliCat = (cmbCliCat.SelectedIndex)
+        '--------------------------------------
+        Write(9, CliID)
+        Write(9, CliName)
+        Write(9, CliAdress)
+        Write(9, CliPhone)
+        Write(9, CliCat)
+        Write(9, vbCrLf)
+        MsgBox("done")
+
+
+    End Sub
+
+    Private Sub cmbCliCat_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbCliCat.SelectedIndexChanged
+        Dim x As String
+        'MsgBox(cmbCliCat.Text)
+        'x = (cmbCliCat.Items.Item(1))
+        'MsgBox(x)
+        ' MsgBox(cmbCliCat.SelectedIndex)
+    End Sub
+
+    Private Sub cmdReformat_Click(sender As Object, e As EventArgs) Handles cmdReformat.Click
+        FileClose(9)
+        FileOpen(10, CliFile, OpenMode.Input)
+        Dim newm As String
+        While Not EOF(10)
+            Input(10, CliID)
+            Input(10, CliName)
+            Input(10, CliAdress)
+            Input(10, CliPhone)
+            Input(10, CliCat & vbCrLf)
+            Input(10, vbCrLf)
+            names(CliName, "ali", "yazbek", " ", newm)
+        End While
+    End Sub
+End Class
---
+++ MIS440 - Ali Yazbeck/Sorting44/WindowsApp_Sorting/Form1.vb
@@ -0,0 +1,114 @@
+﻿Public Class Form1
+
+    Dim theArray(100000) As Integer
+    Dim num As Integer
+
+    '----------------------------------------Load--------------------------------------------------------
+
+    Private Sub cmdLoad_Click(sender As Object, e As EventArgs) Handles cmdLoad.Click
+
+        Dim i As Integer
+        num = CInt(txtLimit.Text)
+        ReDim Preserve theArray(num)
+        If num < 1 Then
+            MsgBox("please fill in the limit textbox")
+            Exit Sub
+        End If
+        For i = 0 To num
+            theArray(i) = Rnd() * 100000
+        Next
+
+        Console.WriteLine("Loaded")
+    End Sub
+
+    '-------------------------------------------Print--------------------------------------------------
+
+    Private Sub cmdPrint_Click(sender As Object, e As EventArgs) Handles cmdPrint.Click
+
+        Dim i As Integer
+        Console.Clear()
+        For i = 1 To num
+            Console.WriteLine(thearray(i))
+        Next
+
+    End Sub
+
+    '---------------------------------------------Selection Sort------------------------------------------
+
+    Private Sub cmdSelection_Click(sender As Object, e As EventArgs) Handles cmdSelection.Click
+
+        Dim top, i, temp As Integer
+        For top = 0 To num - 1
+            For i = top + 1 To num
+                If thearray(i) < thearray(top) Then
+                    temp = thearray(top)
+                    thearray(top) = thearray(i)
+                    thearray(i) = temp
+                End If
+            Next
+        Next
+
+        Console.WriteLine("Sorted")
+    End Sub
+
+    '---------------------------------------------Bubble up Sort----------------------------------------
+
+    Private Sub cmdBubble_Click(sender As Object, e As EventArgs) Handles cmdBubble.Click
+
+        Dim swaps, i, temp As Integer
+        swaps = 1
+        While swaps > 0
+            swaps = 0
+            For i = 0 To num - 1
+                If thearray(i) > thearray(i + 1) Then
+                    temp = thearray(i + 1)
+                    thearray(i + 1) = thearray(i)
+                    thearray(i) = temp
+                    swaps = swaps + 1
+                End If
+            Next
+        End While
+
+        Console.WriteLine("Sorted")
+    End Sub
+
+    '----------------------------------------VB Sort--------------------------------------------
+    Private Sub cmdVB_Click(sender As Object, e As EventArgs) Handles cmdVB.Click
+
+        Array.Sort(theArray)
+
+        MsgBox(theArray(3))
+        Console.WriteLine("Sorted")
+    End Sub
+
+    '------------------------------------------Binary Search---------------------------------------------
+
+    Private Sub cmdBinary_Click(sender As Object, e As EventArgs) Handles cmdBinary.Click
+
+        Dim found As Boolean
+        Dim numVal, low, high, mid, location As Integer
+        low = 1
+        high = num
+        found = False
+        numVal = CInt(txtVal.Text)
+        While (Not found) And (high > low)
+            mid = (low + high) \ 2
+            If thearray(mid) = numVal Then
+                found = True
+                location = mid
+            Else
+                If thearray(mid) > numVal Then
+                    high = mid
+                Else
+                    low = mid
+                End If
+            End If
+        End While
+        If found Then
+            txtResult.Text = location
+        Else
+            txtResult.Text = "Not found"
+        End If
+    End Sub
+
+End Class
