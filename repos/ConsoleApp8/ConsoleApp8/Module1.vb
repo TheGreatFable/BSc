6b2bf7f39b953ca6be3941a507a156adcdd97bdf
Ali Yazbek <59251976+TheGreatFable@users.noreply.github.com>
2022-02-08 22:50:38 +0200

    Add files via upload

---
+++ repos/ConsoleApp8/ConsoleApp8/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+    'quadratic equation
+    Sub Main()
+        Dim a As Single, b As Single, c As Single, delta As Integer
+        a = Console.ReadLine
+        b = Console.ReadLine
+        c = Console.ReadLine
+        If a = 0 Then
+            Console.WriteLine("this is not a second degree equation")
+            End
+        Else
+            delta = b ^ 2 - 4 * a * c
+            If delta < 0 Then
+                Console.WriteLine("no solution")
+            Else 'delta>=0
+                If delta = 0 Then
+                    Console.WriteLine(-b / (2 * a))
+                Else
+                    Console.WriteLine(-b - delta ^ 0.5 / (2 * a))
+                    Console.WriteLine(-b + delta ^ 0.5 / (2 * a))
+                End If
+            End If
+        End If
+        MsgBox(":)")
+    End Sub
+
+End Module
---
+++ repos/ConsoleApp9/ConsoleApp9/Module1.vb
@@ -0,0 +1,38 @@
+﻿Module Module1
+    '2 ex
+    Sub Main()
+        Dim w As Integer
+        Console.WriteLine(fibo(w))
+        temperature()
+        MsgBox(":)")
+    End Sub
+    Sub temperature()
+        Dim t As Single
+        Console.WriteLine("input t")
+        t = Console.ReadLine
+        If t < 0 Then
+            Console.WriteLine("ICE")
+        Else
+            If t <= 100 Then
+                Console.WriteLine("LIQUID")
+            Else
+                Console.WriteLine("STEAM")
+            End If
+        End If
+    End Sub
+    Function fibo(n As Integer) As Integer
+        Dim s1 As Integer, s2 As Integer, i As Integer, result As Integer
+        result = 0
+        s1 = 1
+        s2 = 1
+        Console.WriteLine("input n")
+        n = Console.ReadLine
+        For i = 3 To n
+            result = s1 + s2
+            s1 = s2
+            s2 = result
+        Next
+        Console.WriteLine("result:")
+        Return result
+    End Function
+End Module
